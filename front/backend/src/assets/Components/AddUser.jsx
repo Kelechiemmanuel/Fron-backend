688a3aac20300b08be5b2373c5548dd3d8dc12fb
kelechiemmanuel <ekelechi413@gmail.com>
2026-03-26 12:33:08 +0100

    done

--- front/backend/src/assets/Components/AddUser.jsx
+++ front/backend/src/assets/Components/AddUser.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import axios from "axios"
+import { useState } from "react"
 
 const AddUser = () => {
-  return (
-    <div>AddUser</div>
-  )
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const res = await axios.post(
+            'http://localhost:4800/users', form);
+
+        console.log("SUCCESS:", res.data);
+
+    } catch (error) {
+        console.log("REAL ERROR:", error.response?.data);
+    }
+    console.log("FORM DATA:", form);
+};
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Name"
+                value={form.name}
+                onChange={(e) => setForm({ ...form, name: e.target.value })}
+            />
+
+            <input
+                type="email"
+                placeholder="Email"
+                value={form.email}
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+
+            <input
+                type="password"
+                placeholder="Password"
+                value={form.password}
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+            />
+            <button type="submit">Add User</button>
+        </form>
+    )
 }
 
 export default AddUser
--- front/backend/src/assets/Components/Users.jsx
+++ front/backend/src/assets/Components/Users.jsx
@@ -3,32 +3,32 @@ import { useEffect, useState } from 'react';
 
 const Users = () => {
     const [user, setUser] = useState([]);
+
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await axios.get('https://user-credentials-2czx.vercel.app/')
-                setTimeout(() => {
-                    setUser(response.data)
-                }, 1000);
+                const response = await axios.get('http://localhost:4800/users');
+                setUser(response.data);
             } catch (error) {
-                console.log("message", error);                
+                console.log("REAL ERROR:", error.response?.data);
             }
-        }
-        fetchUsers()
-    })
-  return (
-    <div>
-        <h1>All Users</h1>
-        {Users.map((user, index) => (
-            <p key={index}>
-                {user.name} <br />
-                {user.email} <br />
-                {user.password}
-            
-            </p>
-        ))}
-    </div>
-  )
-}
+        };
 
-export default Users
+        fetchUsers();
+    }, []);
+
+    return (
+        <div>
+            <h1>All Users</h1>
+            {user.map((user, index) => (
+                <p key={index}>
+                    {user.name} <br />
+                    {user.email} <br />
+                    {user.password}
+                </p>
+            ))}
+        </div>
+    );
+};
+
+export default Users;
