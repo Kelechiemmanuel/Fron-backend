5a59ea46c4d89e6bb34f394710d546c6753028c4
kelechiemmanuel <ekelechi413@gmail.com>
2026-03-26 13:55:01 +0100

    done

--- front/backend/src/assets/Components/Users.jsx
+++ front/backend/src/assets/Components/Users.jsx
@@ -20,13 +20,28 @@ const Users = () => {
     return (
         <div>
             <h1>All Users</h1>
-            {user.map((user, index) => (
-                <p key={index}>
-                    {user.name} <br />
-                    {user.email} <br />
-                    {user.password}
-                </p>
-            ))}
+       <div className="p-20">
+          <table className="min-w-full border border-gray-300">
+            <thead>
+              <tr className="bg-amber-200">
+                <th className="py-2 px-4 border">#</th>
+                <th className="py-2 px-4 border">Name</th>
+                <th className="py-2 px-4 border">Email</th>
+                <th className="py-2 px-4 border">Password</th>
+              </tr>
+            </thead>
+            <tbody>
+              {user.map((user, index) => (
+                <tr key={index} className="text-center hover:bg-amber-100">
+                  <td className="py-2 px-4 border">{index + 1}</td>
+                  <td className="py-2 px-4 border">{user.name}</td>
+                  <td className="py-2 px-4 border">{user.email}</td>
+                  <td className="py-2 px-4 border">{user.password}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         </div>
     );
 };
